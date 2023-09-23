9abd9f68967be7cf59fa118a05961b7a9b3e854f
VaibhavPattanshetti <145839183+VaibhavPattanshetti@users.noreply.github.com>
2023-09-23 15:30:47 +0530

    Create matrix_add.c

---
+++ matrix_add.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+int main()
+{
+    int a[3][3],b[3][3],add[3][3],i,j,k;
+     
+     printf("enter matrix a\n");
+     for(i=0;i<3;i++)
+     {
+         for(j=0;j<3;j++)
+         {
+             scanf("%d",&a[i][j]);
+         }
+     }
+     
+     printf("enter matrix b\n");
+     for(i=0;i<3;i++)
+     {
+         for(j=0;j<3;j++)
+         {
+             scanf("%d",&b[i][j]);
+         }
+     }
+     
+     
+     for(i=0;i<3;i++)
+     {
+         for(j=0;j<3;j++)
+         {
+             add[i][j]=0;
+         }
+     }
+     
+     
+    for(i=0;i<3;i++)
+     {
+         for(j=0;j<3;j++)
+         {  
+        
+             add[i][j]=a[i][j] + b[i][j];
+             
+         }
+     } 
+    
+    printf("addition of  matrix a and b :\n");
+     for(i=0;i<3;i++)
+     {
+         for(j=0;j<3;j++)
+         {
+             printf("%d\t",add[i][j]);
+         }
+         printf("\n");
+     }
+     
+     return 0;
+    
+}
